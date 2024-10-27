9900c47c32707e671200bbc72109eb404af87d8a
Basit <basitabdullah1122@gmail.com>
2024-10-27 13:53:09 +0530

    Update 1.4

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,6 +18,7 @@ const setCookies = async (res, token) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    sameSite: "None",
   }); // storing cookie
 };
 
