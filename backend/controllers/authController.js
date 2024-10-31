3ead381ed6ac13c2b5d55d0b21b7dab8969b897a
Basit <basitabdullah1122@gmail.com>
2024-10-31 18:38:24 +0530

    Fix 1

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -87,7 +87,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("jwtAccessToken");
+    res.clearCookie("jwtAccessToken", {
+      httpOnly: true,
+      secure: true, 
+      sameSite: "None",
+    });
     res.status(200).json({
       message: "Logged Out Successfully!",
     });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://meteor-ecom-mern.onrender.com/api",//temp
+  // baseURL: "https://meteor-ecom-mern.onrender.com/api",
+  baseURL: "https://meteor-ecom-mern.onrender.com/api",
   withCredentials: true, // send cookies to the server
 });
 
