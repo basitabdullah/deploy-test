0b36325b6f9553fc32d9e130dcd401d98f04782d
Basit <basitabdullah1122@gmail.com>
2024-11-25 19:35:37 +0530

    backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "https://meteor-ecom-mern.onrender.com/api",
-  baseURL: "https://meteor-ecom-mern.onrender.com/api",
+  baseURL: "https://deploy-test-5cig.onrender.com/api",
   withCredentials: true, // send cookies to the server
 });
 
