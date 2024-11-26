d58963550a8cd62d37978d46cc791b45f850c6e1
Basit <basitabdullah1122@gmail.com>
2024-11-26 12:21:12 +0530

    Frontend Fix.

--- frontend/src/components/Loaders/maxLoader/Loader.jsx
+++ frontend/src/components/Loaders/maxLoader/Loader.jsx
@@ -4,11 +4,6 @@ const Loader = () => {
   return (
     <div className="loader-container">
       <span className="loader"></span>
-      <p className="alert">
-        <span>Alert :</span> This web application is deployed on a{" "}
-        <span>(free-tier)</span> of render deployments so the initial loading can
-        take upto <span>100-150</span> seconds.
-      </p>
     </div>
   );
 };
